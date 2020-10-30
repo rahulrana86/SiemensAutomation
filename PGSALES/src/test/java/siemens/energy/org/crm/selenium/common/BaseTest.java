447b9f1bdf6b9b0c71fee0a587cc0d6ba516cc78
z003r03h <z003r03h@md2kmydc.ad001.siemens.net>
2020-10-31 00:45:53 +0530

    test

--- PGSALES/src/test/java/siemens/energy/org/crm/selenium/common/BaseTest.java
+++ PGSALES/src/test/java/siemens/energy/org/crm/selenium/common/BaseTest.java
@@ -145,21 +145,21 @@ public class BaseTest
 		
 		options.setHeadless(true);
 		//options.addArguments("--headless");
-	    options.addArguments("--disable-gpu");
-	    options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-	    options.addArguments("--ignore-certificate-errors");
+	    //options.addArguments("--disable-gpu");
+	    //options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+	    //options.addArguments("--ignore-certificate-errors");
 	    //options.addArguments("--incognito");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("disable-infobars");
-		options.addArguments("--disable-notifications");
+        //options.addArguments("--disable-popup-blocking");
+        //options.addArguments("disable-infobars");
+		//options.addArguments("--disable-notifications");
 		
 	
-		options.addArguments("start-maximized"); 
-	    options.addArguments("enable-automation"); 
-	    options.addArguments("--no-sandbox"); 
+		//options.addArguments("start-maximized"); 
+	    //options.addArguments("enable-automation"); 
+	    //options.addArguments("--no-sandbox"); 
 	  
-	    options.addArguments("--disable-dev-shm-usage"); 
-	    options.addArguments("--disable-browser-side-navigation"); 
+	    //options.addArguments("--disable-dev-shm-usage"); 
+	    //options.addArguments("--disable-browser-side-navigation"); 
 	 
 		//options.setPageLoadStrategy(PageLoadStrategy.NONE);
 		//options.setCapability(ChromeOptions.CAPABILITY, options);
